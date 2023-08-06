e732b3d64217b82c76940f0c7f813f76a3961d32
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-08-06 20:06:45 +0300

    added navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Navbar.scss'
-
+import { AiOutlineSearch } from 'react-icons/ai'
+import { BiCategoryAlt } from 'react-icons/bi'
+import { FiSettings } from 'react-icons/fi'
+import { IoMdNotificationsOutline } from 'react-icons/io'
+import { PiBracketsSquare } from 'react-icons/pi'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <img src="https://cdn.freelogodesign.org/files/6893ce27b466438c804197b139b1d178/thumb/logo_200x200.png?v=0" alt="" />
+        <h3>Emr1s Admin</h3>
+      </div>
+      <div className="right">
+        <div className="icons">
+          <AiOutlineSearch />
+          <BiCategoryAlt />
+          <PiBracketsSquare />
+          <div className="notification">
+            <IoMdNotificationsOutline />
+            <span>1</span>
+          </div>
+        </div>
+        <div className="settings">
+          <div className="info">
+            <img src="https://www.stylevore.com/wp-content/uploads/2019/06/Sonya-Rudskaya-La-imagen-puede-contener-1-persona-primer-plano.jpg" alt="" />
+            <p>Oleg</p>
+          </div>
+          <FiSettings />
+        </div>
+      </div>
+    </div>
   )
 }
 
