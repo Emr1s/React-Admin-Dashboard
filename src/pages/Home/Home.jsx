aaf1a8b4acd799b9e6ee7c967938d4e097daeae7
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-08-06 20:06:30 +0300

    created grid homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,17 @@ import './Home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="div div1">div1</div>
+        <div className="div div2">div2</div>
+        <div className="div div3">div3</div>
+        <div className="div div4">div4</div>
+        <div className="div div5">div5</div>
+        <div className="div div6">div6</div>
+        <div className="div div7">div7</div>
+        <div className="div div8">div8</div>
+        <div className="div div9">div9</div>
+    </div>
   )
 }
 
